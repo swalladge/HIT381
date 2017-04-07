8cb44aad93b05f28ed7238b55e9e810967811dc2
Samuel Walladge <samuel@swalladge.id.au>
2017-04-07 13:03:02 +0930

    show number of devices

--- functional_prototype/src/Pages.elm
+++ functional_prototype/src/Pages.elm
@@ -20,7 +20,7 @@ header = div [] [ text "Electricity Alert" ]
 
 welcome : List Device -> Html Msg
 welcome d = div []
-    [ div [] [ text "welcome" ]
+    [ div [] [ text "Welcome to Electricity Alert!" ]
     , button [ onClick Home ] [ text "Get Started!" ]
     ]
 
@@ -29,8 +29,11 @@ home : List Device -> Html Msg
 home d = div []
     [
       header
-    , div [] [ text "status" ]
-    , div [] [ text "power draw" ]
+    , div [] [
+        text "status: "
+      , text ((toString (List.length d)) ++ " appliance" ++ (if (List.length d) == 1 then "" else "s"))
+      ]
+    -- , div [] [ text "power draw" ]
     , div [] [
         device_list d
       , button [ onClick AddDevice ] [ text "Add Appliance" ]
@@ -39,12 +42,14 @@ home d = div []
     ]
 
 
+device_list : List Device -> Html Msg
 device_list devices = div [] (List.map (\d -> div [] [ text d.name ]) (List.sortBy .name devices))
 
+add_device : List Device -> Html Msg
 add_device devices = div []
   [
     header
-  , div [] [ text "add device" ]
+  , div [] [ text "Add Appliance" ]
   , label [] [
       text "Name: "
     , input [ type_ "text", placeholder "Name", onInput UpdateName ] []
